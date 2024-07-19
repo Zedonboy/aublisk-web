c17790f43f9d4eb23f9f4aac4a880cd0bc43863c
Declan Nnadozie <zedonbiz@gmail.com>
2024-07-19 12:24:25 +0100

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <header>
+   
     <div class="flex justify-end items-center py-2 px-4 bg-gray-50 lg:px-6 lg:hidden dark:bg-gray-700">
         <a href="#" class="mr-2 text-sm font-medium text-gray-500 hover:underline dark:text-gray-400">Talk to sales</a>
         <a href="#" class="inline-flex items-center p-2 text-sm font-medium text-gray-500 rounded-lg dark:text-gray-400 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 dark:hover:bg-gray-600 focus:outline-none dark:focus:ring-gray-500">
@@ -17,7 +18,7 @@
             <div class="flex col-span-2 justify-end items-center lg:order-3 lg:col-span-1">
                 
                 <!-- <a href="#" class="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Login</a> -->
-                <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Sign In With Internet Identity</a>
+                <!-- <a href="#" class="text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2 lg:py-2.5 mr-2 dark:bg-primary-600 dark:hover:bg-primary-700 focus:outline-none dark:focus:ring-primary-800">Sign In With Internet Identity</a> -->
                 <button data-collapse-toggle="mobile-menu-2" type="button" class="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mobile-menu-2" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
@@ -46,8 +47,8 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-12 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7 xl:col-span-8">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Borderless Digital Pool of knowledge</h1>
-            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">We are a content/book/article sharing platform, where content creators, authors, intellectuals write about what they know and sell it as NFTs. its a Borderless Digital content marketplace</p>
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">Revolutionizing Freelancing with Web3 Power</h1>
+            <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">Unlock guaranteed gigs, instant crypto payments, and verifiable credentials in our decentralized freelancer community. Join the future of work where your skills are valued, your earnings are secure, and your voice shapes the platform. Subscribe now to be first in line for exclusive opportunities and early access!</p>
             <form action="#" class="">
                 <div class="flex items-center mb-3">
                    
@@ -58,9 +59,10 @@
                 
             </form>
         </div>
-        <div class="hidden lg:mt-0 lg:col-span-5 xl:col-span-4 lg:flex">
-            <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/mobile-app.svg" alt="phone illustration">
-        </div>                
+        <div class="ml-embedded w-full lg:mt-0 lg:col-span-5 xl:col-span-4 lg:flex" data-form="YFTNG0"></div>
+        <!-- <div class="hidden ">
+            
+        </div>                 -->
     </div>
 </section>
 
@@ -69,36 +71,49 @@
 <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div class="max-w-screen-md mb-8 lg:mb-16">
-            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Developed for Technocrat and Intellectuals</h2>
-            <p class="text-gray-500 sm:text-xl dark:text-gray-400">A better way to publish and read, A marketplace and ecosystem where knowledge is exchanged, and creativity is monetized through the innovative use of NFT technology</p>
+            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Developed for all Web3 Freelancer</h2>
+            <p class="text-gray-500 sm:text-xl dark:text-gray-400">A better way to work on web3 projects</p>
         </div>
         <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300">
+                        <path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path>
+                        <polyline points="22 4 12 14.01 9 11.01"></polyline>
+                      </svg>
+                    <!-- <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M12 21a9.004 9.004 0 0 0 8.716-6.747M12 21a9.004 9.004 0 0 1-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 0 1 7.843 4.582M12 3a8.997 8.997 0 0 0-7.843 4.582m15.686 0A11.953 11.953 0 0 1 12 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0 1 21 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0 1 12 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 0 1 3 12c0-1.605.42-3.113 1.157-4.418" />
-                      </svg>
+                      </svg> -->
                       
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Borderless</h3>
-                <p class="text-gray-500 dark:text-gray-400">Anyone with crypto can use this, not like kindle's this book is not available for your region</p>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Guaranteed Gigs</h3>
+                <p class="text-gray-500 dark:text-gray-400">Higher probability of securing projects with our Anti-Competition Mechanism and Automatic Job Maker Algorithms</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                        <circle cx="12" cy="12" r="10"></circle>
+                        <line x1="12" y1="8" x2="12" y2="16"></line>
+                        <line x1="8" y1="12" x2="16" y2="12"></line>
+                      </svg>
+                    <!-- <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" >
                         <path stroke-linecap="round" stroke-linejoin="round" d="M18.364 18.364A9 9 0 0 0 5.636 5.636m12.728 12.728A9 9 0 0 1 5.636 5.636m12.728 12.728L5.636 5.636" />
-                      </svg>
+                      </svg> -->
                       
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Anti-Sharing</h3>
-                <p class="text-gray-500 dark:text-gray-400">Your Audience cannot share your books freely, everyone who needs it must buy from you.</p>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Instant Crypto Payments</h3>
+                <p class="text-gray-500 dark:text-gray-400">Receive payments directly in your wallet using popular cryptocurrencies like ckBTC, ckETH, and ckSOL</p>
             </div>
             <div>
                 <div class="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                    
+                    <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <rect x="3" y="11" width="18" height="11" rx="2" ry="2"></rect>
+                        <path d="M7 11V7a5 5 0 0 1 10 0v4"></path>
+                      </svg>
+                    <!-- <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                     -->
                 </div>
-                <h3 class="mb-2 text-xl font-bold dark:text-white">Revenue</h3>
-                <p class="text-gray-500 dark:text-gray-400">Make money, eliminate middlemen, your revenue belong to you alone</p>
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Verifiable Credentials</h3>
+                <p class="text-gray-500 dark:text-gray-400">Automated Resume Writer tracks your progress and rewards, creating portable, blockchain-verified credentials</p>
             </div>
            
         </div>
@@ -113,13 +128,13 @@
             <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">Our team</h2>
             <p class="font-light text-gray-500 sm:text-xl dark:text-gray-400">Explore the great minds behind this app</p>
         </div> 
-        <div class="grid gap-16 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div class="flex justify-center">
             <div class="text-center text-gray-500">
                 <img class="mx-auto mb-4 w-36 h-36 rounded-lg" src="/team1.jpg" alt="Bonnie Avatar">
                 <a href="#">
                     <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Declan Nnadozie</h5>
                 </a>
-                <span>CEO</span>
+                <span>Indie Hacker</span>
                 <div class="flex justify-center mt-4 space-x-4">
                     <!-- <a href="" class="text-[#39569c] hover:text-gray-900 dark:hover:text-white">
                         <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path fill-rule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clip-rule="evenodd" /></svg>
@@ -130,51 +145,14 @@
                     
                 </div>
             </div>
-            <div class="text-center text-gray-500">
-                <img class="mx-auto mb-4 w-36 h-36 rounded-lg" src="/team2.jpg" alt="Jese Avatar">
-                <a href="#">
-                    <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nnadozie Declan</h5>
-                </a>
-                <span>Lead Developer</span>
-                <div class="flex justify-center mt-4 space-x-4">
-                    <a href="https://twitter.com/buildWithDeclan" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
-                    </a>
-                </div>
-            </div>
 
-            <div class="text-center text-gray-500">
-                <img class="mx-auto mb-4 w-36 h-36 rounded-lg" src="/team3.jpg" alt="Jese Avatar">
-                <a href="#">
-                    <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Nnadozie Declan</h5>
-                </a>
-                <span>Lead Designer</span>
-                <div class="flex justify-center mt-4 space-x-4">
-                    <a href="https://twitter.com/buildWithDeclan" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
-                    </a>
-                </div>
-            </div>
-
-            <div class="text-center text-gray-500">
-                <img class="mx-auto mb-4 w-36 h-36 rounded-lg" src="/team4.jpg" alt="Jese Avatar">
-                <a href="#">
-                    <h5 class="mb-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Declan Nnadozie</h5>
-                </a>
-                <span>Lead Blockchain Developer</span>
-                <div class="flex justify-center mt-4 space-x-4">
-                    <a href="https://twitter.com/buildWithDeclan" class="text-[#00acee] hover:text-gray-900 dark:hover:text-white">
-                        <svg class="w-6 h-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true"><path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" /></svg>
-                    </a>
-                </div>
-            </div>
             
            
         </div>  
     </div>
   </section>
 
-  <!-- Newsletter -->
+  <!-- Newsletter
   <section class="bg-white dark:bg-gray-900">
     <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div class="mx-auto max-w-screen-md sm:text-center">
@@ -197,7 +175,7 @@
             </form>
         </div>
     </div>
-  </section>
+  </section> -->
 
   <!-- Footer -->
 
@@ -207,7 +185,7 @@
             <img src="/aublisk_56_logo.png" class="mr-3 h-6 sm:h-9" alt="Aublisk Logo" />
             <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Aublisk</span>
         </a>
-        <p class="my-6 text-gray-500 dark:text-gray-400">The Ecosystem of Technocrats</p>
+        <p class="my-6 text-gray-500 dark:text-gray-400">The Ecosystem of Web3 Freelancers</p>
         <!-- <ul class="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
             <li>
                 <a href="#" class="mr-4 hover:underline md:mr-6 ">About</a>
